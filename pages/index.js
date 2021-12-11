290a5c34259c10e5e80ab8da2f13749caa7f4ef0
Holland Pleskac <hollandpleskac@gmail.com>
2021-12-11 12:30:16 -0800

    integration test

--- pages/index.js
+++ pages/index.js
@@ -23,13 +23,40 @@ const HomePage = () => {
     setContractData()
   }, [])
 
-  const startAndPickWinner = () => {
+  useEffect(() => {
+    if (Lottery !== null && lotteryAddress !== null) {
+      console.log("listing for events", lotteryAddress)
+      const filter = {
+        address: lotteryAddress,
+        topics: [
+          // the name of the event, parnetheses containing the data type of each event, no spaces
+          ethers.utils.id("PickingWinner(bytes32)")
+        ]
+      }
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      provider.on(filter, () => {
+        console.log("picking winner event fired!")
+      })
+    }
+  }, [Lottery, lotteryAddress])
+
+  const startAndPickWinner = async () => {
     const provider = new ethers.providers.Web3Provider(window.ethereum)
     const signer = provider.getSigner();
     const lottery = new ethers.Contract(lotteryAddress, Lottery.abi, signer)
 
-    
+    const startTx = await lottery.startLottery()
+    await startTx.wait()
+    console.log("started lottery")
 
+    // Enter participants
+    const options = { value: ethers.utils.parseEther("0.05") }
+    const enterLotteryAcc1Tx = await lottery.enterLottery(options)
+    console.log("entered into lottery")
+
+    const endTx = await lottery.endLottery()
+    await endTx.wait()
+    // console.log("ended the lottery")
   }
 
   const getRandomNumber = async () => {
@@ -38,6 +65,9 @@ const HomePage = () => {
 
     const result = await lottery.randomResult()
     console.log("random result: ", result);
+
+    const lastWiner = await lottery.lastWinner()
+    console.log("last winner", lastWiner)
   }
 
 
---
+++ test/integration/lottery-integration-test.js
@@ -0,0 +1,59 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+const { networkConfig } = require("../../helper-hardhat-config");
+
+describe("Lottery Unit Test", function () {
+
+  let lottery
+
+  beforeEach(async function () {
+    this.timeout(120000);
+    const networkName = hre.network.name
+    console.log(`Deploying to ${networkName} network`)
+
+    // Deploy Lottery Contract
+    const Lottery = await hre.ethers.getContractFactory("Lottery")
+    lottery = await Lottery.deploy(
+      networkConfig[networkName].ethUsdPriceFeed,
+      networkConfig[networkName].vrfCoordinator,
+      networkConfig[networkName].linkToken,
+      networkConfig[networkName].fee,
+      networkConfig[networkName].keyHash
+    )
+    await lottery.deployed()
+    console.log("Lottery deployed at address", lottery.address)
+
+    // ---------- Fund Lottery Contract
+    await hre.run("fund-link", { contract: lottery.address, linkaddress: networkConfig[networkName].linkToken })
+    console.log("Successfully funded link")
+  });
+
+  it("can pick winner correctly", async function () {
+    this.timeout(300000);
+    // Start Lottery
+    const startTx = await lottery.startLottery()
+    await startTx.wait()
+    console.log("started lottery")
+
+    // // Enter participants
+    const options = { value: ethers.utils.parseEther("0.05") }
+    const enterTx = await lottery.enterLottery(options)
+    await enterTx.wait()
+    console.log("entered the lottery")
+
+    // End Lottery
+    const endTx = await lottery.endLottery()
+    await endTx.wait()
+    console.log("ended the lottery")
+
+    await new Promise(r => setTimeout(r, 60000));
+
+    // Get last winner
+    const lastWinner = await lottery.lastWinner()
+    console.log("lastWinner: ", lastWinner)
+
+    const [account] = await ethers.getSigners()
+    expect(lastWinner).to.equal(account.address)
+
+  })
+});
