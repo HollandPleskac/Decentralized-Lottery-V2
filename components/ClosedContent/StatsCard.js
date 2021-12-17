5867524b14a81a6e688194911531f618c90a2716
Holland Pleskac <hollandpleskac@gmail.com>
2021-12-17 15:53:54 -0800

    styles

--- components/ClosedContent/StatsCard.js
+++ components/ClosedContent/StatsCard.js
@@ -6,7 +6,7 @@ import { faUserTie } from '@fortawesome/free-solid-svg-icons'
 
 const StatsCard = ({ players, ether }) => {
   return (
-    <div className='absolute top-8 right-8 w-40 p-5 flex flex-col bg-white shadow' >
+    <div className='absolute top-8 right-8 w-40 p-5 flex flex-col bg-white shadow rounded-md' >
       <p className='mb-2 text-center' >Lottery Stats</p>
       <hr />
       <div className='flex items-center mt-2' >
